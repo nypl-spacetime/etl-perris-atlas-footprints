704312b20c078b8442df9610af9e138cd6ef1298
Bert Spaan <bert.spaan@gmail.com>
2017-04-20 13:01:39 -0400

    addresses!

--- perris-atlas-footprints.js
+++ perris-atlas-footprints.js
@@ -41,58 +41,96 @@ const newProperties = R.fromPairs(PROPERTIES.map((property) => Array.isArray(pro
 
 // Building [1] -> [n] Address [1] -> [1] Street
 
+function makeAddressObject(buildingId, type, year, address) {
+  if (address) {
+    const addressId = `${buildingId}-${type}`
+
+    const addressName = (address) => {
+      let name = address.street
+      if (address.number) {
+        name = `${address.number} ${name}`
+      }
+      return name
+    }
+
+    return [
+      {
+        type: 'object',
+        obj: {
+          id: addressId,
+          type: 'st:Address',
+          name: addressName(address),
+          validSince: year,
+          validUntil: year,
+          data: Object.assign(address, {
+            addressType: type
+          })
+        }
+      },
+      {
+        type: 'relation',
+        obj: {
+          from: addressId,
+          to: buildingId,
+          type: 'st:in'
+        }
+      }
+    ]
+  }
+}
+
 function writeFeature (writer, feature, callback) {
-  var properties = R.fromPairs(R.toPairs(R.pick(pickProperties, feature.properties))
+  const properties = R.fromPairs(R.toPairs(R.pick(pickProperties, feature.properties))
     .filter((pair) => pair[1])
     .map((pair) => [newProperties[pair[0]], pair[1]]))
 
   BOOLEANS.forEach((boolean) => properties[boolean] === properties[boolean])
 
+  const buildingId = feature.properties.id
   const year = parseInt(feature.properties.layer_year)
 
-  var address
+  let address
   if (feature.properties.street) {
     address = {
-      address: {
-        street: feature.properties.street
-      }
+      street: feature.properties.street,
+      number: feature.properties.number
     }
 
     if (feature.properties.number) {
-      address.address.number = feature.properties.number
+      address.number = feature.properties.number
     }
   }
 
-  var secondaryAddress
+  let secondaryAddress
   if (feature.properties.secondar_1) {
     secondaryAddress = {
-      secondaryAddress: {
-        street: feature.properties.secondar_1
-      }
+      street: feature.properties.secondar_1
     }
 
     if (feature.properties.secondar_2) {
-      secondaryAddress.secondaryAddress.number = feature.properties.secondar_2
+      secondaryAddress.number = feature.properties.secondar_2
     }
   }
 
-  var building = {
+  const building = {
     type: 'object',
     obj: {
-      id: feature.properties.id,
+      id: buildingId,
       type: 'st:Building',
+      name: feature.properties.name || undefined,
       validSince: year,
       validUntil: year,
-      data: Object.assign(properties, address, secondaryAddress),
+      data: Object.assign(properties, {address}, {secondaryAddress}),
       geometry: rewind(feature.geometry, false)
     }
   }
 
-  if (feature.properties.name) {
-    building.obj.name = feature.properties.name
-  }
+  const addresses = [
+    makeAddressObject(buildingId, 'primary', year, address),
+    makeAddressObject(buildingId, 'secondary', year, secondaryAddress)
+  ]
 
-  writer.writeObject(building, callback)
+  writer.writeObjects(R.flatten([building, addresses]), callback)
 }
 
 function downloadFile (dir, filename, callback) {
